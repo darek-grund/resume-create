73d9e9f99cee4a1179c6ab34af189c224950cbc1
Darek Grund <darek.grund@gmail.com>
2019-05-22 22:24:18 +0200

    Add label to TextField and TextArea

--- src/components/TextArea/index.js
+++ src/components/TextArea/index.js
@@ -5,16 +5,23 @@ import styles from './TextArea.module.css';
 const TextArea = props => {
   const { placeholder } = props;
   const [hasFocus, setHasFocus] = useState(false);
+  const [value, setValue] = useState('');
 
   const className = [styles.textArea];
+
   if (hasFocus) {
     className.push(styles.focus);
   }
+  if (value) {
+    className.push(styles.dirty);
+  }
   return (
     <div className={className.join(' ')}>
+      <label className={styles.label}>{placeholder}</label>
       <textarea
         className={styles.input}
-        placeholder={placeholder}
+        value={value}
+        onInput={event => setValue(event.target.value)}
         onFocus={() => setHasFocus(true)}
         onBlur={() => setHasFocus(false)}
       />
--- src/components/TextField/index.js
+++ src/components/TextField/index.js
@@ -5,17 +5,24 @@ import styles from './TextField.module.css';
 const TextField = props => {
   const { placeholder } = props;
   const [hasFocus, setHasFocus] = useState(false);
+  const [value, setValue] = useState('');
 
   const className = [styles.textField];
+
   if (hasFocus) {
     className.push(styles.focus);
   }
+  if (value) {
+    className.push(styles.dirty);
+  }
   return (
     <div className={className.join(' ')}>
+      <label className={styles.label}>{placeholder}</label>
       <input
         type="text"
         className={styles.input}
-        placeholder={placeholder}
+        value={value}
+        onInput={event => setValue(event.target.value)}
         onFocus={() => setHasFocus(true)}
         onBlur={() => setHasFocus(false)}
       />
